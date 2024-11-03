a80fad4af2dda028a9e662cbdac6b79219e8c703
DiamondGotCat <diamondgotcat@DiamondGotCatnoMacBook-Air.local>
2024-11-03 11:57:09 +0900

    Change Design.

--- shortfrom/text/process/index.php
+++ shortfrom/text/process/index.php
@@ -1,138 +1,131 @@
 <!DOCTYPE html>
-<html lang="ja">
-<head>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>T2U - Kamu Japan</title>
-  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/tailwindcss/2.2.19/tailwind.min.css">
-  <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-5309485435511985" crossorigin="anonymous"></script>
-  <style>
-    body {
-      font-family: 'Segoe UI', Tahoma, Geneva, Verdana, sans-serif;
-      background-color: #f3f4f6;
-      display: flex;
-      flex-direction: column;
-      min-height: 100vh;
-    }
-    header {
-      background-color: #3b82f6;
-      color: white;
-      padding: 1rem;
-    }
-    .container {
-      max-width: 1024px;
-      margin: 0 auto;
-      padding: 0 1rem;
-    }
-    nav ul {
-      list-style: none;
-      padding: 0;
-      margin: 0;
-    }
-    nav ul li {
-      display: inline-block;
-      margin-right: 1rem;
-    }
-    nav ul li:last-child {
-      margin-right: 0;
-    }
-    nav ul li a {
-      color: white;
-      text-decoration: none;
-    }
-    section {
-      padding: 2rem 0;
-    }
-    #apps {
-      display: flex;
-      justify-content: center;
-    }
-    form {
-      display: flex;
-      align-items: center;
-      background-color: white;
-      border-radius: 1rem;
-      padding: 0.5rem;
-      box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
-    }
-    input[type="url"] {
-      border: none;
-      padding: 0.5rem;
-      border-radius: 0.5rem;
-      flex: 1;
-      margin-right: 0.5rem;
-    }
-    button[type="submit"] {
-      background-color: #3b82f6;
-      color: white;
-      border: none;
-      padding: 0.5rem 1rem;
-      border-radius: 0.5rem;
-      cursor: pointer;
-    }
-    button[type="submit"]:hover {
-      background-color: #2563eb;
-    }
-    footer {
-      background-color: #333;
-      color: white;
-      padding: 1rem 0;
-      text-align: center;
-      margin-top: auto;
-    }
-  </style>
-</head>
-<body>
-<header>
-  <div class="container">
-    <h1>Kamu Japan</h1>
-    <nav>
-      <ul>
-        <li>T2U</li>
-        <li><a href="https://kamu.jp/" class="text-blue">Home</a></li>
-        <li><a href="https://kamu.jp/t/" class="text-blue">ShortID to Text</a></li>
-      </ul>
-    </nav>
-  </div>
-</header>
-
-<section id="apps">
-  <div class="container">
-    <div class="container bg-gray-100 rounded-lg p-8 flex justify-center">
-      <?php
-      require_once('../config/db.php');
-
-      if ($_SERVER['REQUEST_METHOD'] === 'GET') {
-          $url = $_GET['url'];
-
-          // 9桁のランダムなアルファベットと数字で構成されるIDを生成
-          $shortId = uniqid();
-
-          // データベースに保存
-          $insertQuery = "INSERT INTO short_urls (url, short_id) VALUES (?, ?)";
-          $stmt = $mysqli->prepare($insertQuery);
-          $stmt->bind_param('ss', $url, $shortId);
-          $stmt->execute();
-
-          // 短縮URLを表示
-          $shortUrl = "https://kamu.jp/t?q=$shortId";
-          echo "<a href='$shortUrl' target='_blank'>生成されたURL: $shortUrl</a>";
-
-          // ステートメントを閉じる
-          $stmt->close();
+<html lang="en">
+  <head>
+      <meta charset="UTF-8">
+      <meta name="viewport" content="width=device-width, initial-scale=1.0">
+      <title>from Text - KAMU</title>
+      <link href="https://cdn.jsdelivr.net/npm/daisyui@4.12.14/dist/full.min.css" rel="stylesheet" type="text/css" />
+      <script src="https://cdn.tailwindcss.com"></script>
+      <link rel="preconnect" href="https://fonts.googleapis.com">
+      <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+      <link href="https://fonts.googleapis.com/css2?family=M+PLUS+2:wght@100..900&display=swap" rel="stylesheet">
+      <style>
+      body {
+        font-family: "M PLUS 2", system-ui;
+        font-optical-sizing: auto;
+        font-weight: 400;
+        font-style: normal;
       }
+      .m-plus-2-black {
+        font-family: "M PLUS 2", system-ui;
+        font-optical-sizing: auto;
+        font-weight: 900;
+        font-style: normal;
+      }
+      </style>
+  </head>
+<body>
 
-      // MySQLi 接続を閉じる
-      $mysqli->close();
-      ?>
+  <header>
+
+    <div class="navbar bg-base-100">
+      <div class="navbar-start">
+        <div class="dropdown">
+          <div tabindex="0" role="button" class="btn btn-ghost btn-circle">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              class="h-5 w-5"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke="currentColor">
+              <path
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                stroke-width="2"
+                d="M4 6h16M4 12h16M4 18h7" />
+            </svg>
+          </div>
+          <ul
+            tabindex="0"
+            class="menu menu-sm dropdown-content bg-base-100 rounded-box z-[1] mt-3 w-52 p-2 shadow">
+            <li><a href="/">HOME</a></li>
+          </ul>
+        </div>
+      </div>
+      <div class="navbar-center">
+        <a href="/" class="btn btn-ghost text-xl">KAMU</a>
+      </div>
+      <div class="navbar-end">
+        <!-- Navbar End -->
+      </div>
     </div>
-  </div>
-</section>
 
-<footer>
-  <div class="container">
-    <p>&copy; 2024 TechCat56. All rights reserved.</p>
+  </header>
+
+  <main>
+
+    <div class="hero bg-base-200 min-h-screen">
+      <div class="hero-content text-center">
+        <div class="max-w-md">
+            <h1 class="text-5xl font-bold">from Text</h1>
+
+            <div class="divider"></div>
+            
+            <form action="../process" method="GET">
+                <div class="flex basis-0">
+                    <input type="text" name="url" placeholder="Enter Text..." class="input input-bordered w-full max-w-xs" />
+                    <button type="submit" class="btn btn-primary">Create</button>
+                </div>
+            </form>
+
+            <?php
+              require_once('../config/db.php');
+
+              if ($_SERVER['REQUEST_METHOD'] === 'GET') {
+                  $url = $_GET['url'];
+
+                  // 9桁のランダムなアルファベットと数字で構成されるIDを生成
+                  $shortId = uniqid();
+
+                  // データベースに保存
+                  $insertQuery = "INSERT INTO short_urls (url, short_id) VALUES (?, ?)";
+                  $stmt = $mysqli->prepare($insertQuery);
+                  $stmt->bind_param('ss', $url, $shortId);
+                  $stmt->execute();
+
+                  // 短縮URLを表示
+                  $shortUrl = "https://kamu.jp/t?q=$shortId";
+                  echo "<a href='$shortUrl' target='_blank'>$shortUrl</a>";
+
+                  // ステートメントを閉じる
+                  $stmt->close();
+              }
+
+              // MySQLi 接続を閉じる
+              $mysqli->close();
+            ?>
+
+            <div class="divider">OR</div>
+
+            <a href="/t/"><button class="btn btn-primary">Use</button></a>
+
+        </div>
+      </div>
+    </div>
+
   </div>
-</footer>
+
+  </main>
+
+  <footer>
+
+    <footer class="footer footer-center bg-base-200 text-base-content rounded p-10">
+      <aside>
+        <p>Copyright &copy;2024 - All right reserved by DiamondGotCat</p>
+      </aside>
+    </footer>
+
+  </footer>
+
 </body>
 </html>
